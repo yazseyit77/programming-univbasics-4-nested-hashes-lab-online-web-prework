76c0767a4761b35ab95f9f0427c6d7b621b7b68c
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-03 19:55:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ def adding_matz
         }
      }
 
-		 programmer_hash[:yukihiro_matsumoto] = {:known_for, :languages}
+		 programmer_hash[:yukihiro_matsumoto] = {:known_for :languages}
 end
 
 def changing_alan
